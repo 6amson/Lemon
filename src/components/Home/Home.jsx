d5d8e782bbfce98e00459b013c8f8e1502840fb3
bunmigrey <bunmigrey@icloud.com>
2024-01-18 18:56:06 +0100

    services offered section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,15 @@
-import { Container, Header, Button, Ul, HeroContainer } from "../../styles/homestyle";
+import { Container, Header, Button, Ul, HeroContainer, BrandContainer, Servicesoffered, RiskAssessment } from "../../styles/homestyle";
 import logohome from "../../images/logohome.svg";
-import menuIcon from "../../images/menuIcon.svg"
+import menuIcon from "../../images/menuIcon.svg";
+import airbnb from "../../images/airbnb.svg";
+import cocacola from "../../images/cocacola.svg";
+import slack from "../../images/slack.svg";
+import soundcloud from "../../images/soundcloud.svg";
 import "../../styles/homestyle.scss";
+import coinLemon from "../../images/coinLemon.png";
+import teamLemon from "../../images/teamLemon.png"
+import phoneLemon from "../../images/phoneLemon.png"
+
 
 
 export default function Home() {
@@ -20,16 +28,47 @@ export default function Home() {
                         <Button>Login</Button>
                         <Button>Signup</Button>
                     </div>
-                    <img src={menuIcon} alt="menuicon" width="35px" height="35px" id="menuIcon"/>
+                    <img src={menuIcon} alt="menuicon" width="35px" height="35px" id="menuIcon" />
                 </Header>
                 <HeroContainer>
                     <div>
                         <p>Lets' help you grow your assets.</p>
                         <p>At<span>Lemon,</span>we have the best hands to manage your portfolio.</p>
-                       <Button>Get started</Button>
+                        <Button>Get started</Button>
                     </div>
                     <div id="heroPage--right"></div>
                 </HeroContainer>
+                <BrandContainer>
+                    <p>Brands trust us, you can too.</p>
+                    <div>
+                        <img src={airbnb} alt="airbnb" width="64px" height="64px" />
+                        <img src={cocacola} alt="cocacola" width="64px" height="64x" />
+                        <img src={soundcloud} alt="soundcloud" width="64px" height="64px" />
+                        <img src={slack} alt="slack" width="64px" height="64px" />
+                    </div>
+                </BrandContainer>
+                <Servicesoffered>
+                    <div>
+                        <p>Effortlessly take control of your assets with our all-in-one Asset Management App. From tracking inventory and equipment to monitoring financial portfolios, our platform provides a comprehensive solution for efficient asset management.</p>
+                        <img src={phoneLemon} alt="phone 3d illustration" width="230px" height="200px" />
+                    </div>
+                    <div>
+                        <img src={teamLemon} alt="team 3d illustration" width="250px" height="200px" />
+                        <p>We go beyond traditional asset management. Our team brings strategic insights, leveraging market trends and data analytics to make informed decisions that drive positive outcomes for your portfolio.</p>
+                    </div>
+                    <div>
+                        <p>We take pride in offering an asset management approach that goes beyond conventional strategies, ensuring not only the preservation but the consistent growth of your investments harnessing the power of data analytics.</p>
+                        <img src={coinLemon} alt="coin 3d illustration" width="200px" height="200px" />
+                    </div>
+                </Servicesoffered>
+                <RiskAssessment>
+                    <div>
+                        
+                    </div>
+                    <div>
+
+                    </div>
+                </RiskAssessment>
             </Container>
         </>
     )
--- src/styles/homestyle.jsx
+++ src/styles/homestyle.jsx
@@ -16,7 +16,6 @@ export const Header = styled.header`
     height: 80px;
     width: 100vw;
     top: -8px;
-    // border-bottom: solid #323143 1px;
     box-shadow: .5px .5px 8px .5px #323143;
     display: flex;
     align-items: center;
@@ -32,6 +31,10 @@ export const Header = styled.header`
     &:hover{
         cursor: pointer;
     }
+    }
+
+    @media (max-width: 500px) {
+        height: 64px:
     }
 `
 export const Ul = styled.ul`
@@ -103,7 +106,7 @@ export const HeroContainer = styled.div`
     margin-top: 128px;
     width: 100vw;
    display: grid;
-   padding-left: 64px;
+   padding-left: 40px;
    padding-right: 32px;
    grid-template-columns: repeat(2, 1fr);
    font-family: ClashDisplay-Variable; 
@@ -113,6 +116,11 @@ export const HeroContainer = styled.div`
         font-size: 32px;
         margin-top: 64px;
         margin-bottom: 32px;
+
+        @media (max-width: 500px) {
+            font-size: 28px;
+        }
+
         }
 
         p:nth-child(2){
@@ -122,7 +130,6 @@ export const HeroContainer = styled.div`
 
             @media (max-width: 500px) {
             font-size: 50px;
-                
             }
                 span{
                     margin-left: 20px;
@@ -140,7 +147,98 @@ export const HeroContainer = styled.div`
 
    @media (max-width: 750px) {
     grid-template-columns: 1fr;
-    margin-top: 32px;
+   grid-template-rows: repeat(2, 1fr);
+    margin-top: 16px;
+    margin-bottom: 64px;
   }
 
+`
+
+export const BrandContainer = styled.div`
+margin-top: 128px;
+margin-bottom: 64px;
+width: 100vw;
+
+
+p{
+    text-align: center;
+    font-family: BespokeSerif-Variable;
+    font-size: 32px;
+    color: grey;
+    font-weight: 500;
+
+    @media (max-width: 500px) {
+        font-size: 24px;
+    }
+}
+ 
+div{
+    display: flex;
+  justify-content: space-around;
+  opacity: 0.8;
+  padding: 16px;
+
+  img{
+    filter: grayscale(100%);
+    opacity: 0.8;
+
+    @media (max-width: 500px) {
+       width: 48px;
+       height: 48px;
+    }
+  }
+}
+
+@media (max-width: 500px) {
+    margin-top: 8px;
+ }
+
+`
+
+export const Servicesoffered = styled.div`
+ background: black;
+ height: fir-content;
+ padding-left: 32px;
+ padding-right: 16px;
+ color: whitesmoke;
+ width: 100vw;
+
+ div{
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 16px;
+    font-family: ClashDisplay-Variable; 
+    justify-content: space-around;
+    align-items: center;
+    padding-top: 16px;
+    padding-bottom: 16px;
+
+    p{
+        width: 50%;
+        font-size: 20px;
+
+        @media (max-width: 500px) {
+            font-size: 16px;
+        }
+    }
+
+    img{
+        @media (max-width: 500px) {
+            width: 50%;
+        }
+    }
+
+    @media (max-width: 800px) {
+        margin-bottom: 32px;
+        padding-top: 16px;
+        padding-bottom: 16px;
+    }
+ }
+`
+
+export const RiskAssessment = styled.div`
+ display: flex;
+ flex-wrap: wrap;
+ width: 100vw;
+ 
 `
